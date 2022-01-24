0b6aeede9706e72f588350404b7425a6961b19a5
eric-nichols-nyc <ebn646@gmail.com>
2022-01-24 12:35:10 -0500

    update to app.js

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.use('/api/activities/', activityHandler)
 app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build/index.html'))
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build')))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
